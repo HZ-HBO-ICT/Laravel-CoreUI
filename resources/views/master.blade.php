dc89a4730e7854ca34608e1e4f835068f7394097
Loek van der Linde <lind0096@hz.nl>
2018-11-19 16:06:32 +0100

    Add very ugly menu implementation. Needs to be refactored ASAP.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -48,93 +48,55 @@
     <div class="sidebar">
         <nav class="sidebar-nav">
             <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.html">
-                        <i class="nav-icon fa fa-chart-bar"></i> Dashboard
-                    </a>
-                </li>
-                <li class="nav-title">First section</li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="nav-icon fa fa-cog"></i> Settings</a>
-                </li>
-                <li class="nav-title">Second section</li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#">
-                        <i class="nav-icon fa fa-arrow-down"></i> Dropdown</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://fontawesome.com/v5.3.1/icons?d=gallery">
-                                <i class="nav-icon icon-star"></i> FA Icons
-                                <span class="badge badge-success">NEW</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+                @foreach(config('coreui.menu') as $item)
+                    @if(is_string($item))
+                        <li class="nav-title">{{ $item }}</li>
+                    @else
+                        @if(isset($item['submenu']))
+                            <li class="nav-item nav-dropdown">
+                                <a class="nav-link nav-dropdown-toggle" href="#">
+                                    @if(isset($item['icon'])) <i class="nav-icon fa fa-{{ $item['icon'] }}"></i> @endif
+                                    {{ $item['text'] }}
+                                </a>
+                                <ul class="nav-dropdown-items">
+                                    {{-- TODO Write this into a function with the menu builder --}}
+                                    {{--<li class="nav-item nav-dropdown">--}}
+                                        {{--<a class="nav-link nav-dropdown-toggle" href="#">--}}
+                                            {{--<i class="nav-icon fa fa-arrow-down"></i> Dropdown</a>--}}
+                                        {{--<ul class="nav-dropdown-items">--}}
+                                            {{--<li class="nav-item">--}}
+                                                {{--<a class="nav-link" href="https://fontawesome.com/v5.3.1/icons?d=gallery">--}}
+                                                    {{--<i class="nav-icon icon-star"></i> FA Icons--}}
+                                                    {{--<span class="badge badge-success">NEW</span>--}}
+                                                {{--</a>--}}
+                                            {{--</li>--}}
+                                        {{--</ul>--}}
+                                    {{--</li>--}}
+                                </ul>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a
+                                    @if(isset($item['url'])) href="{{ url($item['url']) }}" @endif
+                                    @if(isset($item['route'])) href="{{ route($item['route']) }}" @endif
+                                    @if(isset($item['target'])) target="{{ $item['target'] }}" @endif
+                                    class="nav-link"
+                                >
+                                    @if(isset($item['icon'])) <i class="nav-icon fa fa-{{ $item['icon'] }}"></i> @endif
+                                    {{ $item['text'] }}
+                                </a>
+
+                            </li>
+                        @endif
+                    @endif
+                @endforeach
             </ul>
         </nav>
         <button class="sidebar-minimizer brand-minimizer" type="button"></button>
     </div>
     <main class="main">
-        <!-- Breadcrumb-->
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-
         <div class="container-fluid">
-            <div class="row">
-                <div class="col">
-                    <h1>Hello world!</h1>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col">
-                    <div class="card text-center">
-                        <div class="card-header">
-                            Featured
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">It's a broader card with text below as a natural lead-in to extra content. This content is a little longer.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card">
-                        <img class="card-img-top" src="https://via.placeholder.com/300x180" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">You can also use headers, like this image!</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                        <div class="card-footer">
-                            Cards can even have footers
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="card text-white bg-warning mb-3">
-                        <div class="card-header">Header</div>
-                        <div class="card-body">
-                            <h5 class="card-title">Secondary card title</h5>
-                            <p class="card-text">You can use a couple of color classes to make your cards more visually appealing too!</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card border-danger mb-3">
-                        <div class="card-header">Header</div>
-                        <div class="card-body text-danger">
-                            <h5 class="card-title">Danger card title</h5>
-                            <p class="card-text">Or make use of some slightly less intense coloring, which can convey your intentions well enough too.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @yield('body')
         </div>
     </main>
 </div>
