a41f95810b2388348dccbd61ba5abd05a5b79b29
Loek van der Linde <lind0096@hz.nl>
2018-11-19 16:06:13 +0100

    Add example menu

--- config/coreui.php
+++ config/coreui.php
@@ -17,6 +17,37 @@ return [
      * Menu builder
      */
     'menu' => [
-
+        [
+            'text' => 'Dashboard',          // The text to be displayed inside the menu.
+            'url' => 'admin/dashboard',     // The URL behind the text. Mutually exclusive with "route" option.
+            'icon' => 'chart-bar',          // Name of FontAwesome icon to display.
+            'target' => '_blank',           // Target attribute of <a> tag.
+        ],
+        'First section',                    // Section header
+        [
+            'text' => 'Users',
+//            'route' => 'admin.users',       // The route behind the text. Mutually exclusive with "url" option.
+            'icon' => 'users'
+        ],
+        'Admin only',
+        [
+            'text' => 'Settings',
+            'icon' => 'cog',
+            'submenu' => [
+                [
+                    'text' => 'Level one',
+                    'url' => 'admin/settings/level-one'
+                ],
+                [
+                    'text' => 'Level two',
+                    'submenu' => [
+                        [
+                            'text' => 'Add as many as you like',
+                            'url' => '#'
+                        ]
+                    ]
+                ]
+            ]
+        ]
     ]
 ];
