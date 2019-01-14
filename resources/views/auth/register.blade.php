c36e456b02727c0a8016856d4e344f3631130979
Loek van der Linde <lind0096@hz.nl>
2019-01-14 13:32:57 +0100

    Update dependencies

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,42 +15,46 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <div class="card mx-4">
-                <div class="card-body p-4">
-                    <h1>Register</h1>
-                    <p class="text-muted">Create your account</p>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                        <span class="input-group-text">
-                        <i class="fa fa-user"></i>
-                        </span>
-                        </div>
-                        <input class="form-control" type="text" placeholder="Username">
+                <div class="card-group">
+                    <div class="card-body p-4">
+                        <h1>Register</h1>
+                        <p class="text-muted">Create your account</p>
+
+                        <form action="{{ route('register') }}" method="post">
+                            @csrf
+
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-user"></i></span>
+                                </div>
+
+                                <input class="form-control" type="text" placeholder="Username">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-envelope"></i></span>
+                                </div>
+
+                                <input class="form-control" type="text" placeholder="Email">
+                            </div>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                                </div>
+
+                                <input class="form-control" type="password" placeholder="Password">
+                            </div>
+                            <div class="input-group mb-4">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fa fa-redo"></i></span>
+                                </div>
+
+                                <input class="form-control" type="password" placeholder="Repeat password">
+                            </div>
+
+                            <button class="btn btn-block btn-success" type="button">Create Account</button>
+                        </form>
                     </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">
-                            <i class="fa fa-envelope"></i>
-                            </span>
-                        </div>
-                        <input class="form-control" type="text" placeholder="Email">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">
-                            <i class="fa fa-lock"></i>
-                            </span>
-                        </div>
-                        <input class="form-control" type="password" placeholder="Password">
-                    </div>
-                    <div class="input-group mb-4">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">
-                            <i class="fa fa-redo"></i>
-                            </span>
-                        </div>
-                        <input class="form-control" type="password" placeholder="Repeat password">
-                    </div>
-                    <button class="btn btn-block btn-success" type="button">Create Account</button>
                 </div>
             </div>
         </div>
