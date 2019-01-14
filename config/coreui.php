5c4c182aebd130383605c50df5cb104315e81de2
Loek van der Linde <lind0096@hz.nl>
2019-01-14 14:31:20 +0100

    Comment out menu config to prevent routing exceptions in clean projects

--- config/coreui.php
+++ config/coreui.php
@@ -17,40 +17,40 @@ return [
      * Menu builder
      */
     'menu' => [
-        [
-            'text' => 'Dashboard',          // The text to be displayed inside the menu.
-            'url' => 'admin/dashboard',     // The URL behind the text. Mutually exclusive with "route" option.
-            'icon' => 'chart-bar',          // Name of FontAwesome icon to display.
-            'target' => '_blank',           // Target attribute of <a> tag.
-        ],
-        'First section',                    // Section header
-        [
-            'text' => 'Users',
+//        [
+//            'text' => 'Dashboard',          // The text to be displayed inside the menu.
+//            'url' => 'admin/dashboard',     // The URL behind the text. Mutually exclusive with "route" option.
+//            'icon' => 'chart-bar',          // Name of FontAwesome icon to display.
+//            'target' => '_blank',           // Target attribute of <a> tag.
+//        ],
+//        'First section',                    // Section header
+//        [
+//            'text' => 'Users',
 //            'route' => 'admin.users',       // The route behind the text. Mutually exclusive with "url" option.
-            'icon' => 'users'
-        ],
-        'Admin only',
-        [
-            'text' => 'Settings',
-            'icon' => 'cog',
-            'submenu' => [
-                [
-                    'text' => 'Level one',
-                    'icon' => 'bell',       // Tip: always set icons. Submenus in particular tend to get misaligned plus it's more accessible and user friendly.
-                    'url' => 'admin/settings/level-one'
-                ],
-                [
-                    'text' => 'Level two',
-                    'icon' => 'clock',
-                    'submenu' => [
-                        [
-                            'text' => 'Add as many as you like',
-                            'url' => '#'
-                        ]
-                    ]
-                ]
-            ]
-        ]
+//            'icon' => 'users'
+//        ],
+//        'Admin only',
+//        [
+//            'text' => 'Settings',
+//            'icon' => 'cog',
+//            'submenu' => [
+//                [
+//                    'text' => 'Level one',
+//                    'icon' => 'bell',       // Tip: always set icons. Submenus in particular tend to get misaligned plus it's more accessible and user friendly.
+//                    'url' => 'admin/settings/level-one'
+//                ],
+//                [
+//                    'text' => 'Level two',
+//                    'icon' => 'clock',
+//                    'submenu' => [
+//                        [
+//                            'text' => 'Add as many as you like',
+//                            'url' => '#'
+//                        ]
+//                    ]
+//                ]
+//            ]
+//        ]
     ],
 
     /**
