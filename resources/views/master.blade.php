207929327fc79b4aabb348e2f61a08adff8bf414
Loek van der Linde <lind0096@hz.nl>
2019-01-28 15:57:38 +0100

    Fix bug where logout route would rendered even if it shouldn't be

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -26,22 +26,22 @@
         </button>
         <ul class="nav ml-auto">
             <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    @if (\Illuminate\Support\Facades\Auth::user() !== null)
+                @if (\Illuminate\Support\Facades\Auth::user() !== null)
+                    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                         <i class="fa fa-user-cog"></i> {{ \Illuminate\Support\Facades\Auth::user()->name }}
-                    @endif
-                </a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <div class="dropdown-header text-center">
-                        <strong>Settings</strong>
-                    </div>
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        <i class="fas fa-sign-out-alt"></i> Logout
                     </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <div class="dropdown-header text-center">
+                            <strong>Settings</strong>
+                        </div>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="fas fa-sign-out-alt"></i> Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                @endif
             </li>
         </ul>
     </div>
