f5cf2176b7809bf0e9dbc01d4dbf1ec5ff9d070c
dwaard <daan.de.waard@hz.nl>
2019-01-29 12:06:41 +0100

    added myself as contributer and documented the new config setting

--- config/coreui.php
+++ config/coreui.php
@@ -8,7 +8,7 @@ return [
     'title' => 'CoreUI Laravel Theme by HZ-HBO-ICT',
 
     /*
-     * Text to put in the top-left of the menu bar
+     * Text to put in the top-left of the menu bar. logo_mini is shown when the navbar is collapsed.
      * NOTE: This is a non-escaped string, so you can put HTML in here
      */
     'logo' => 'Core<strong>UI</strong>',
