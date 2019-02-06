d37a1ae38df789529d7bcb49e3f358c09b155614
Daniel de Cloet <djdecloet@gmail.com>
2019-02-06 10:59:12 +0100

    Change publish path for views to include the vendor/ folder

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -80,7 +80,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     private function publishViews(): void
     {
         $this->publishes([
-            $this->getPathFromPackageRoot('resources/views/') => resource_path('views/coreui'),
+            $this->getPathFromPackageRoot('resources/views/') => resource_path('views/vendor/coreui'),
         ], 'views');
     }
 
