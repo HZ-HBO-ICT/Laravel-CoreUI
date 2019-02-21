505aae951e44d89fd0473550665f81a7d69479ab
Loek van der Linde <lind0096@hz.nl>
2019-02-21 16:51:38 +0100

    Add gate filter for building menu event

---
+++ src/Menu/Filters/GateFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HzHboIct\LaravelCoreUI\Menu\Filters;
+
+use HzHboIct\LaravelCoreUI\Menu\Builder;
+use Illuminate\Contracts\Auth\Access\Gate;
+
+class GateFilter implements FilterInterface
+{
+    protected $gate;
+
+    public function __construct(Gate $gate)
+    {
+        $this->gate = $gate;
+    }
+
+    public function transform($item, Builder $builder)
+    {
+        if (! $this->isVisible($item)) {
+            return false;
+        }
+
+        return $item;
+    }
+
+    protected function isVisible($item)
+    {
+        if (! isset($item['can'])) {
+            return true;
+        }
+
+        if (isset($item['model'])) {
+            return $this->gate->allows($item['can'], $item['model']);
+        }
+
+        return $this->gate->allows($item['can']);
+    }
+}
