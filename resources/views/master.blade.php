17c32838b3e64955e7a082a29d1e206fb31e3524
Albvadi <randolf.bcgp@gmail.com>
2019-10-08 13:26:36 +0200

    Add csrf-token to the master template

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- CoreUI CSS -->
     <link rel="stylesheet" href="{{ asset('vendor/coreui/css/coreui.min.css') }}">
