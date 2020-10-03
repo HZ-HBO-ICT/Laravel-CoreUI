be929ead1c82c37d4921097ac6a570f6bfce37ac
Daniel-I-Am <djdecloet@gmail.com>
2020-10-03 16:54:06 +0200

    Update blade files to new structure

--- config/coreui.php
+++ config/coreui.php
@@ -12,7 +12,6 @@ return [
      * NOTE: This is a non-escaped string, so you can put HTML in here
      */
     'logo' => 'Core<strong>UI</strong>',
-    'logo_mini' => 'C',
 
     /*
      * Menu builder
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,40 +19,43 @@
 </head>
 <body class="c-app">
 
-{{--<script type="application/javascript" src="{{ asset('vendor/coreui/js/toggle_sidebar.js') }}"></script>--}}
-<div class="c-sidebar c-sidebar-fixed c-sidebar-lg-show">
+<div id="sidebar" class="c-sidebar c-sidebar-fixed c-sidebar-lg-show">
     <a class="c-sidebar-brand d-md-down-none" href="{{ url('/') }}">
         <div class="c-sidebar-brand-full">{!! config('coreui.logo') !!}</div>
-        <div class="c-sidebar-brand-minimized">{!! config('coreui.logo_mini') !!}</div>
     </a>
 
     <ul class="c-sidebar-nav">
         @each('coreui::menu-item', $coreUI->menu(), 'item')
     </ul>
-
-    <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-unfoldable"></button>
-
-{{--    <script type="application/javascript" src="{{ asset('vendor/coreui/js/add_listener_to_sidebar.js') }}"></script>--}}
 </div>
 
 <div class="c-wrapper">
-    <header class="c-header c-header-fixed navbar">
-        <button class="c-header-toggler sidebar-toggler d-lg-none mr-auto" type="button" data-toggle="sidebar-show">
+    <header class="c-header c-header-fixed justify-content-between">
+        <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show">
             <span class="c-header-toggler-icon"></span>
         </button>
-        <button class="c-header-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
+        <button class="c-header-toggler c-class-toggler mfs-3 d-lg-none" type="button" data-target="#sidebar" data-class="c-sidebar-show">
             <span class="c-header-toggler-icon"></span>
         </button>
-        <ul class="c-header-nav ml-auto">
+
+        <ul class="c-header-nav mr-4">
             @yield('account', View::make('coreui::account'))
         </ul>
+
+        {{-- Only render the subheader if the current view has *any* breadcrumbs --}}
+        @if(View::hasSection('breadcrumb'))
+        <div class="c-subheader justify-content-between px-3">
+            <div aria-label="breadcrumb" role="navigation">
+                <ol class="breadcrumb border-0 m-0 px-0 px-md-3">
+                    @yield('breadcrumb')
+                </ol>
+            </div>
+        </div>
+        @endif
     </header>
 
     <div class="c-body">
         <main class="c-main">
-            <div aria-label="breadcrumb">
-                @yield('breadcrumb')
-            </div>
             <div class="container-fluid">
                 @yield('body')
             </div>
